92bf7f65ad3cc6ca1f440e05b90c34a3f05ba46e
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-04-09 22:59:09 -0400

    installed settings.local.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -752,17 +752,8 @@ $settings['entity_update_backup'] = TRUE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => 'drupaldb',
-  'username' => 'drupal',
-  'password' => 'verybadpassword',
-  'prefix' => '',
-  'host' => 'db',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+ }
+
 $settings['config_sync_directory'] = 'sites/default/files/config_lbpBCL3u-wHU2-klqwjNQFHLZN0rWVAwGvwDogbKl8Ysn2owMJ6p9HVqKxCLbCJONxY_nOqAwg/sync';
